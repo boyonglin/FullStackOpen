89556b32c274a552929e5d02f7f8039f82642f93
Clancy Lin <poerevan9@gmail.com>
2024-10-12 00:05:35 +0800

    Exercises 1.1.-1.2.

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,64 @@
+import './App.css'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      - {props.name}: {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+  return (
+    <>
+      <p>Number of exercises: {total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
